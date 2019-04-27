293c511755968255446c5aff41ca26f0f7e5665b
RedBeastDigital <50056004+RedBeastDigital@users.noreply.github.com>
2019-04-27 13:10:38 -0500

    Add files via upload

---
+++ Contact.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="vent.css" type="text/css"/> 
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Contact</title>
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+    </head>
+    <body>
+        <header>
+            <h1 align="center">
+                <a href="index.php" class="btn btn-info" role="button">Home</a>
+                <a href="login.php" class="btn btn-info" role="button">Login</a>
+                <a href="login.php" class="btn btn-info" role="button">Sing in</a>
+                <a href="Aboutofus.php" class="btn btn-info" role="button">About of us</a>
+                <a href="login.php" class="btn btn-info" role="button">Contact</a>
+             </h1>
+                </header>
+        <aside>
+            <p>
+            </p>
+        </aside>
+        <section>
+            <article>
+                <header>
+                    <h2>Información Personal</h2>
+                    <p>
+                       ....
+                    </p>
+                </header>
+                <p>
+                    ....
+                </p>
+            </article>
+        </section>
+        <footer>
+            <p>
+                ....
+            </p>
+        </footer>
+            <!--  </div> -->
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+    </body>
+</html>
